9d052e3db68081d1cfee240d31ad2817856b84e3
Manuel <manuel.buenstorf@gmail.com>
2026-03-04 15:59:22 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,18 +14,21 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   'morhetz/gruvbox',
   'nvim-tree/nvim-tree.lua',
-  'nvim-lua/plenary.nvim',
-  'nvim-telescope/telescope.nvim',
   'echasnovski/mini.nvim',
   'nvim-treesitter/nvim-treesitter',
   -- 'williamboman/mason.nvim',
   -- 'williamboman/mason-lspconfig.nvim',
   -- 'neovim/nvim-lspconfig',
   'lewis6991/gitsigns.nvim',
-  {'lukas-reineke/indent-blankline.nvim', main = "ibl", opts = {}},
   'folke/flash.nvim',
   'nvim-orgmode/orgmode',
-  'nvim-orgmode/telescope-orgmode.nvim',
+  {'chomosuke/typst-preview.nvim', lazy = false, opts = {}},
+  'stevearc/oil.nvim',
+  'folke/snacks.nvim',
+  'Wansmer/treesj',
+  "ibhagwan/fzf-lua",
+  "0xzhzh/fzf-org.nvim",
+  "kdheepak/lazygit.nvim",
 })
 
 require('mini.basics').setup()
@@ -33,16 +36,17 @@ require('mini.basics').setup()
 -- VIM SETTINGS --------------------------
 local ok, _ = pcall(vim.cmd, 'colorscheme gruvbox')
 vim.o.relativenumber  = true
-vim.o.tabstop         = 2
-vim.o.shiftwidth      = 2
-vim.o.expandtab       = true
-vim.o.smartindent     = true
+vim.o.tabstop         = 2 -- How many spaces is a tab
+vim.o.shiftwidth      = 2 -- How far to indent with <>
+vim.o.expandtab       = true -- Use spaces for tab
+vim.o.smartindent = true
 
 -- VIM KEYMAPS ---------------------------
 --copy paste cut
 vim.keymap.set('v', '<leader>c', '"+y')
 vim.keymap.set('v', '<leader>x', '"+d')
 vim.keymap.set('n', '<leader>v', '"+p')
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 --splits
 vim.keymap.set('n', '<leader>ö', '<C-W><C-L>')
@@ -51,40 +55,62 @@ vim.keymap.set('n', '<leader>k', '<C-W><C-J>')
 vim.keymap.set('n', '<leader>j', '<C-W><C-H>')
 
 -- resize splits
-vim.keymap.set('n', '<leader>>', ':vertical res +15<CR>')
-vim.keymap.set('n', '<leader><', ':vertical res -15<CR>')
 vim.keymap.set('n', '<leader>sh', ':split<CR>')
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>')
 
 -- buffer navigation
 vim.keymap.set('n', '<leader>i', ':bnext<CR>')
 vim.keymap.set('n', '<leader>u', ':bprevious<CR>')
-vim.keymap.set('n', '<leader>q', ':bd<CR>')
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('t', '<C-h>', '<C-\\><C-n>')
 
 -- NVIMTREE -----------------------------
 require("nvim-tree").setup()
 vim.keymap.set('n', '<leader>t', ':NvimTreeFindFileToggle<CR>')
 
--- TELESCOPE -----------------------------
-require "telescope".setup({
-  defaults = {
-    borderchars = {"", "", "", "", "", "", "", ""},
-    layout_config = {
-      height = 100,
-      width = 400
-    }
-  }
+-- Snacks ------------------------------
+require("snacks").setup({
+  indent = {
+    enabled = true,
+    animate = {enabled = false},
+    -- scope = {enabled = false},
+  },
+  toggle = {
+    enabled = true
+  },
 })
-local telescope = require('telescope')
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fgs', builtin.git_status, {})
-vim.keymap.set('n', '<leader>fgc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>fgb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+vim.keymap.set('n', '<leader>q', Snacks.bufdelete.delete)
+
+-- Oil ----------------------------------
+require("oil").setup({keymaps = {["<Esc>"] = { "actions.parent", mode = "n" },}})
+Snacks.toggle.new({
+  name = "Oil",
+  id = "toggle_oil",
+  get = function()
+    return vim.bo.filetype == 'oil'
+  end,
+  set = function(state)
+    if state then
+      vim.cmd('Oil')
+    else
+      require("oil.actions").close.callback()
+    end
+  end,
+}):map("<leader>e")
+
+-- TELESCOPE -----------------------------
+require("fzf-lua").setup({
+  keymap = {
+    fzf = { ["ctrl-q"] = "select-all+accept"},
+  },
+  winopts = {
+    fullscreen = true,
+    border = "none",
+    preview = { border = "none"},
+  },
+})
+vim.keymap.set('n', '<leader>ff', FzfLua.files, {})
+vim.keymap.set('n', '<leader>fs', FzfLua.live_grep_native, {})
+vim.keymap.set('n', '<leader>fgs', FzfLua.git_status, {})
 
 -- FLASH ---------------------------------
 vim.keymap.set('n', '<leader>g', function() require("flash").jump() end, {})
@@ -103,12 +129,16 @@ require('mini.completion').setup {
   }
 }
 
+-- TREESJ -------------------------------
+require('treesj').setup({use_default_keymaps=false})
+vim.keymap.set('n', '<leader>sj', require('treesj').toggle)
+
 -- GITSIGNS ------------------------------
 require('gitsigns').setup()
 
 -- TREESITTER ----------------------------
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"c", "cpp", "lua", "vim", "vimdoc", "query", "python", "go", "html", "css", "javascript", "puppet", "terraform", "yaml", "odin" },
+  ensure_installed = {"c", "cpp", "lua", "query", "python", "go", "html", "css", "javascript", "puppet", "terraform", "yaml", "odin", "typst", "bash", "json", "helm"},
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
@@ -123,16 +153,11 @@ require'nvim-treesitter.configs'.setup {
 -- local lspconfig = require("lspconfig")
 -- lspconfig.ols.setup {}
 
--- IBL ------------------------------------
-require('ibl').setup{
-  indent = {highlight = {"NonText", "NonText"},},
-  scope = {enabled=false},
-}
 
 -- ORGMODE --------------------------------
 require('orgmode').setup({
   org_agenda_files = '~/orgfiles/**/*',
-  org_default_notes_file = '~/orgfiles/refile.org',
+  org_default_notes_file = '~/orgfiles/inbox.org',
   org_todo_keywords = {'TODO', 'WAIT', 'HOLD', '|', 'DONE', 'CANC'},
   org_todo_keyword_faces = {
     TODO = ':foreground #282828 :background #cc241d :weight bold',
@@ -149,7 +174,7 @@ require('orgmode').setup({
   org_log_into_drawer = 'LOGBOOK',
   org_startup_folded = 'content',
   org_hide_leading_stars = true,
-  org_hide_emphasis_markers = true,
+  -- org_hide_emphasis_markers = true,
   org_startup_indented = true,
   org_tags_column = 0,
   mappings = {
@@ -159,9 +184,23 @@ require('orgmode').setup({
   },
 })
 
-vim.keymap.set('n', '<leader>or', require("telescope").extensions.orgmode.refile_heading)
-vim.keymap.set('n', '<leader>of', telescope.extensions.orgmode.search_headings)
+require("fzf-org").setup()
+vim.keymap.set('n', '<leader>of', require("fzf-org").orgmode, {})
+vim.keymap.set('n', '<leader>or', require("fzf-org").refile_to_headline, {})
 
--- Fix bug of telescope-orgmode.nvim where files have folds on opening
-vim.o.foldlevelstart  = 99
-vim.o.conceallevel    = 2
+--- LazyGit
+vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>')
+
+-- Custom functions and commands
+
+-- Run kustomization
+vim.api.nvim_create_user_command("Kust", function()
+  local dir = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p:h")
+  local output = vim.fn.systemlist("kustomize build " .. vim.fn.shellescape(dir))
+
+  vim.cmd("vsplit | enew")
+  vim.bo.buftype, vim.bo.bufhidden, vim.bo.swapfile, vim.bo.filetype =
+    "nofile", "wipe", false, "yaml"
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, output)
+end, { desc = "Kustomize build" })
